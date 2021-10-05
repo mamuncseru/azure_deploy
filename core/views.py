53cf22f314876c31852143d29c86ee6eefc02051
Md. Abdullah Al Mamun <mamun.cseian@gmail.com>
2021-10-05 14:47:00 +0600

    target size changed to 299

--- core/views.py
+++ core/views.py
@@ -25,7 +25,7 @@ def index(request):
         #
         # https://www.tensorflow.org/api_docs/python/tf/keras/preprocessing/image/load_img
         #
-        image = load_img(file_url, target_size=(224, 224))
+        image = load_img(file_url, target_size=(299, 299))
         numpy_array = img_to_array(image)
         image_batch = np.expand_dims(numpy_array, axis=0)
         processed_image = inception_v3.preprocess_input(image_batch.copy())
